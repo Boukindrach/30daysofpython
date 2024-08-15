7914162198df8dbaf68cdbcb3a2b980e301acc10
Boukind Rachid <boukind.r1@gmail.com>
2024-08-15 03:56:58 +0100

    day 2

---
+++ day_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# some math operators
+
+print(3 + 4) # addition (+)
+print(3 - 4) # subtraction (-)
+print(3 / 4) # division (/)
+print(3 * 4) # multiplication (*)
+print(3 ** 4) # exponential (**)
+print(3 % 4) # modulus(%)
+print(3 // 4) # floor divison operator (//)
+
+# Checks the data type of a value
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python','Finland']))
+print(type(True))
+print(type("Morocco"))
+print(type((1, 'hey', 10.10)))
+print(type({"good": 4, "bad": 3}))
+
+# Euclidian distance between (2, 3) and (10, 8)
+
+print(((2-3)**2) + ((10-8)**2))
---
+++ day_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# day two: 30 days of python programming 
+
+first_name = "Rachid"
+country = "Morocco"
+age = 22
+year = 2024
+is_married = False
+
+# declaring multiple variables in one line:
+first_name, country, age = "Rachid", "Morocco", 22
+
+# checking the data types of the variables:
+print(type(first_name))
+print(type(age))
+print(type(is_married))
+
+# getting the length of my first name:
+print("length of my first name: ", len(first_name))
+
+print(abs(len(first_name) - len(country)))
+
+
